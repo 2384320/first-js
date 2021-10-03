fef496770f187f768f6a144fd3a08559812f4f45
6720 <71584474+2384320@users.noreply.github.com>
2021-10-04 02:45:25 +0900

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-const images = [
-    "KYOMI.png",
-    "LANGUAGE.png",
-    "SECOND_LANGUAGE.png"
-];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-document.body.style = `background-image: url("img/${chosenImage}")`;
